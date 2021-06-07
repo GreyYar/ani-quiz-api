ad36817df64cc259da163f5b65d7f804302aa95e
Sergey Spasskiy <greyyar93@mail.ru>
2021-06-07 06:55:35 +0300

    Added swagger, mongoose & mocked endpoint calls

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [],
+  // imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
+  imports: [QuestionModule],
 })
+
 export class AppModule {}
